1a0a27d5a4b09812060118846a003cef0c75982a
elftmjon27 <elftmjon27@7hills.org>
2026-09-24 09:44:19 -0400

    Lemonade Stand Project

---
+++ Lemon.java
@@ -0,0 +1,11 @@
+public class Lemon {
+	public static void main(String[] args) {
+		// Call Lemonade Stand constructor, instance is l1
+		LemonadeStand l1 = new LemonadeStand();
+		// Call the method inside the Lemonade Stand class
+		l1.setUp();
+		
+	}
+}
+
+
---
+++ LemonadeStand.java
@@ -0,0 +1,22 @@
+public class LemonadeStand {
+	// declare variables
+	private double moneyMine;
+	//private double costLemons = 1.50; // 1 lemon
+	//private double costSugar = 2.00; // 1 cup sugar
+	//private double costIce = 0.75; // 1 cup ice
+	private double[] cost = {1.50, 2.00, 0.75};
+	//private int qtyLemons = 0;
+	//private int qtySugar = 0;
+	//private int qtyIce = 0;
+	private double[] qty = {0, 0, 0};
+	// add as many variables you think we need to make and sell cups of lemonade - 
+	private double[] ratio;
+	private int totalCustomers;
+	private int cupsAvail;
+	
+	public void LemonadeStand() {
+		
+	}
+	
+	
+}
